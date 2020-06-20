c118865cb18ceed5e7dc47b1fde9d9ed6a6b0e6c
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-06-20 15:10:54 +0900

    Add files via upload

---
+++ Cp0/0-2_trainning.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main ()
+{
+    int i,j;
+    double d;
+
+    i = 10;
+    j = 20;
+    d = 99.101;
+
+    printf("Here are some values: \n");
+
+    printf("number of a is: %d\n", i); 
+    printf("number of a is: %d\n", j);
+    printf("number of a is: %f\n", d);  
+
+    return 0;
+}
